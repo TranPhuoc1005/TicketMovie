686a241d8cdd0dc368d0b9efbf4d929bdb59b881
phuocth <tranphuoc1005@gmail.com>
2025-08-22 09:08:55 +0700

    fix store

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
-import scheduleSlice from './cinemaDetail.js'
 import authSlice from './auth.slice.js'
 
 export const store = configureStore({
     reducer: {
-        scheduleSlice,
         authSlice
     }
 })
